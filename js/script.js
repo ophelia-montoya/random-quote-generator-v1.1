3e1c72ac8264f5a359f0a74b2bb9da964568f8a0
ophelia-montoya <101679169+ophelia-montoya@users.noreply.github.com>
2022-05-15 21:52:24 -0700

    added detailed comments

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * declared a `quotes` array of objects that include keys:
+ * quote by female writers
+ * a source (the author)
+ * citation: the reference material (i.e. name of book)
+ * tags: type of book
 ***/
 
 const quotes = [
@@ -40,7 +44,9 @@ const quotes = [
 
 
 /***
- * `getRandomQuote` function
+ * declared `getRandomQuote` function
+ * declared randomNum variable that generates a random number from 0 to last index of quotes array
+ * return random quote object using randomNum as index value of array
 ***/
 
 function getRandomQuote() {
@@ -48,24 +54,44 @@ function getRandomQuote() {
   return quotes[randomNum];
   //console.log(randomNum);
   //console.log(quotes[randomNum]);
+
 }
-getRandomQuote();
 
-setInterval(randomBgColor, 2000);
+//call function created above;
+getRandomQuote(); 
 
+//set a 10 second timer so page automatically updates background color
+// referenced https://www.w3schools.com/jsref/met_win_setinterval.asp
+setInterval(randomBgColor, 10000);
+
+/**
+ * declared a function that changes background color
+ * assigned 3 randomly-generated numbers to 'color' variable
+ * @returns an RGB value using the color variable that changes the background color
+ */
 function randomBgColor() {
   let color= `${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256) }`;
   return document.querySelector('body').style.backgroundColor = `RGB(${color})`;
 }
 
+//created a 'click' EventListener that runs the randomBgColor function when you click on the 'show another quote' button
 document.getElementById('load-quote').addEventListener("click", randomBgColor, false);
 
 
-/***
- * `printQuote` function
-***/
-setInterval(printQuote, 2000);
+//set a 10second timer so page automatically runs printQuote function & changes quote displayed 
+//referenced https://www.w3schools.com/jsref/met_win_setinterval.asp
+setInterval(printQuote, 10000);
 
+/***
+ * declared `printQuote` function
+ * assigned returned quote object to variable named randomQuoteObject
+ * created html string using template literal, interpolating the quote & source keys of each quote object to create 2 <p> tags
+ * if conditional statement that checks if quote object has a citation & adds a <span> tag to html string if true
+ * if quote object has a year key, <span> tag with interpolated year is added to html string
+ * EXTRA CREDIT: if quote object has tags key/prop, <span> tag is interpolated w/ tag to html string
+ * closes the 2nd <p> tag we left open to create string
+ * returns html string that is assigned to the innerHTML of the quote-box div
+***/
 function printQuote() { 
   let randomQuoteObject = getRandomQuote();
   let html = `<p class="quote"> ${randomQuoteObject.quote}</p> 
